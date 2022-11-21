22ed317d46a795414159a0d46e1b5d1839adadeb
Wizarphics <wizarphics@gmail.com>
2022-11-21 01:33:05 +0100

    added support for cli request to the request class

--- Request.php
+++ Request.php
@@ -13,11 +13,26 @@
 
 namespace wizarphics\wizarframework;
 
+use RecursiveArrayIterator;
+use RecursiveIteratorIterator;
+use Sage;
+use wizarphics\wizarframework\files\FileCollection;
+use wizarphics\wizarframework\files\FileUploaded;
+
 class Request
 {
+    /**
+     * File collection
+     *
+     * @var FileCollection|null
+     */
+    protected $files;
     public function getPath()
     {
         $path = $_SERVER['REQUEST_URI'] ?? '/';
+        if($this->Method() == 'cli'){
+            $path = $_SERVER['argv'][1]??'/';
+        }
         $position = strpos($path, '?');
         if ($position === false) {
 
@@ -28,33 +43,83 @@ class Request
 
     public function Method()
     {
+        if (is_cli()) {
+            return 'cli';
+        }
         return strtolower($_SERVER['REQUEST_METHOD']);
     }
 
     public function getBody()
     {
         $body = [];
-        if ($this->Method()==='get'){
-            foreach ($_GET as $key=>$value) {
-                $body[$key]=filter_input(INPUT_GET, $key, FILTER_SANITIZE_SPECIAL_CHARS );
+        if ($this->Method() === 'get') {
+            foreach ($_GET as $key => $value) {
+                $body[$key] = filter_input(INPUT_GET, $key, FILTER_SANITIZE_SPECIAL_CHARS);
             }
         }
-        if ($this->Method()==='post'){
-            foreach ($_POST as $key=>$value) {
-                $body[$key]=filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS );
+        if ($this->Method() === 'post') {
+            foreach ($_POST as $key => $value) {
+                if (is_array($value)) {
+                    $body[$key] = [];
+                    foreach ($value as $vKey => $Kvalue) {
+                        $body[$key][$vKey] = filter_var($Kvalue, FILTER_SANITIZE_SPECIAL_CHARS);
+                    }
+                } else {
+                    $body[$key] = filter_input(INPUT_POST, $key, FILTER_SANITIZE_SPECIAL_CHARS);
+                }
             }
         }
 
         return $body;
     }
 
+
+    public function getFiles()
+    {
+        if ($this->files === null) {
+            $this->files = new FileCollection();
+        }
+
+        return $this->files->all(); //return all uploaded files
+    }
+
+    /**
+     * Verify if a file exist, by the name of the input field used to upload it, in the collection
+     * of uploaded files and if is have been uploaded with multiple option.
+     *
+     * @return array|null
+     */
+    public function getFileMultiple(string $fileID)
+    {
+        if ($this->files === null) {
+            $this->files = new FileCollection();
+        }
+
+        return $this->files->getFileMultiple($fileID);
+    }
+
+    /**
+     * Retrieves a single file by the name of the input field used
+     * to upload it.
+     *
+     * @return FileUploaded|null
+     */
+    public function getFile(string $fileID)
+    {
+        if ($this->files === null) {
+            $this->files = new FileCollection();
+        }
+
+        return $this->files->getFile($fileID);
+    }
+
     public function isGet()
     {
-        return $this->Method()==='get';
+        return $this->Method() === 'get';
     }
 
     public function isPost()
     {
-        return $this->Method()==='post';
+        return $this->Method() === 'post';
     }
 }
