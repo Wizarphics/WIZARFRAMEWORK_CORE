a0966404bb3b833868d9d974445f0cbbf31f257e
Wizarphics <wizarphics@gmail.com>
2022-11-21 01:41:37 +0100

    add constants.php for defining required application constants that are available all over the application

---
+++ configs/Constants.php
@@ -0,0 +1,4 @@
+<?php
+
+defined('DEBUG_LOG_PATH') or define('DEBUG_LOG_PATH', ROOT_DIR.'/runtime/logs/debbug_logger/');
+defined('MIGRATION_PATH') or define('MIGRATION_PATH', ROOT_DIR.'/migrations/');
