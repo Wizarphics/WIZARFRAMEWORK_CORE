0a2c8c591f50a6c99c4a9c7dc55903317c2452a8
Wizarphics <wizarphics@gmail.com>
2022-12-08 23:55:07 +0100

    Improve the routing system callback handler

--- Request.php
+++ Request.php
@@ -21,13 +21,28 @@ use wizarphics\wizarframework\files\FileUploaded;
 
 class Request
 {
+
+    /**
+     * @var array
+     */
+    private array $routeArgs;
     /**
      * File collection
      *
      * @var FileCollection|null
      */
     protected $files;
-    public function getPath()
+    /**
+     * [Description for getPath]
+     *
+     * @return string|false
+     * 
+     * Created at: 11/24/2022, 2:25:48 PM (Africa/Lagos)
+     * @author     Wizarphics <wizarphics@gmail.com> 
+     * @see       {@link https://wizarphics.com} 
+     * @copyright Wizarphics 
+     */
+    public function getPath():string|false
     {
         $path = $_SERVER['REQUEST_URI'] ?? '/';
         if($this->Method() == 'cli'){
@@ -41,7 +56,17 @@ class Request
         return substr($path, 0, $position);
     }
 
-    public function Method()
+    /**
+     * [Description for Method]
+     *
+     * @return string
+     * 
+     * Created at: 11/24/2022, 2:26:38 PM (Africa/Lagos)
+     * @author     Wizarphics <wizarphics@gmail.com> 
+     * @see       {@link https://wizarphics.com} 
+     * @copyright Wizarphics 
+     */
+    public function Method():string
     {
         if (is_cli()) {
             return 'cli';
@@ -49,7 +74,17 @@ class Request
         return strtolower($_SERVER['REQUEST_METHOD']);
     }
 
-    public function getBody()
+    /**
+     * [Description for getBody]
+     *
+     * @return array
+     * 
+     * Created at: 11/24/2022, 2:27:34 PM (Africa/Lagos)
+     * @author     Wizarphics <wizarphics@gmail.com> 
+     * @see       {@link https://wizarphics.com} 
+     * @copyright Wizarphics 
+     */
+    public function getBody():array
     {
         $body = [];
         if ($this->Method() === 'get') {
@@ -74,7 +109,17 @@ class Request
     }
 
 
-    public function getFiles()
+    /**
+     * [Description for getFiles]
+     *
+     * @return array|null
+     * 
+     * Created at: 11/24/2022, 2:28:04 PM (Africa/Lagos)
+     * @author     Wizarphics <wizarphics@gmail.com> 
+     * @see       {@link https://wizarphics.com} 
+     * @copyright Wizarphics 
+     */
+    public function getFiles():array|null
     {
         if ($this->files === null) {
             $this->files = new FileCollection();
@@ -83,13 +128,21 @@ class Request
         return $this->files->all(); //return all uploaded files
     }
 
+
     /**
      * Verify if a file exist, by the name of the input field used to upload it, in the collection
      * of uploaded files and if is have been uploaded with multiple option.
      *
+     * @param string $fileID
+     * 
      * @return array|null
+     * 
+     * Created at: 11/24/2022, 2:29:18 PM (Africa/Lagos)
+     * @author     Wizarphics <wizarphics@gmail.com> 
+     * @see       {@link https://wizarphics.com} 
+     * @copyright Wizarphics 
      */
-    public function getFileMultiple(string $fileID)
+    public function getFileMultiple(string $fileID):array|null
     {
         if ($this->files === null) {
             $this->files = new FileCollection();
@@ -102,9 +155,16 @@ class Request
      * Retrieves a single file by the name of the input field used
      * to upload it.
      *
+     * @param string $fileID
+     * 
      * @return FileUploaded|null
+     * 
+     * Created at: 11/24/2022, 2:28:40 PM (Africa/Lagos)
+     * @author     Wizarphics <wizarphics@gmail.com> 
+     * @see       {@link https://wizarphics.com} 
+     * @copyright Wizarphics 
      */
-    public function getFile(string $fileID)
+    public function getFile(string $fileID):FileUploaded|null
     {
         if ($this->files === null) {
             $this->files = new FileCollection();
@@ -113,13 +173,64 @@ class Request
         return $this->files->getFile($fileID);
     }
 
-    public function isGet()
+    /**
+     * [Description for isGet]
+     *
+     * @return bool
+     * 
+     * Created at: 11/24/2022, 2:30:00 PM (Africa/Lagos)
+     * @author     Wizarphics <wizarphics@gmail.com> 
+     * @see       {@link https://wizarphics.com} 
+     * @copyright Wizarphics 
+     */
+    public function isGet():bool
     {
         return $this->Method() === 'get';
     }
 
-    public function isPost()
+    /**
+     * [Description for isPost]
+     *
+     * @return bool
+     * 
+     * Created at: 11/24/2022, 2:30:05 PM (Africa/Lagos)
+     * @author     Wizarphics <wizarphics@gmail.com> 
+     * @see       {@link https://wizarphics.com} 
+     * @copyright Wizarphics 
+     */
+    public function isPost():bool
     {
         return $this->Method() === 'post';
     }
+
+	/**
+	 * [Description for getRouteArgs]
+	 *
+	 * @return array
+	 * 
+	 * Created at: 11/24/2022, 2:30:19 PM (Africa/Lagos)
+	 * @author     Wizarphics <wizarphics@gmail.com> 
+	 * @see       {@link https://wizarphics.com} 
+	 * @copyright Wizarphics 
+	 */
+	public function getRouteArgs(): array {
+		return $this->routeArgs;
+	}
+
+	/**
+	 * [Description for setRouteArgs]
+	 *
+	 * @param array $routeArgs
+	 * 
+	 * @return self
+	 * 
+	 * Created at: 11/24/2022, 2:30:28 PM (Africa/Lagos)
+	 * @author     Wizarphics <wizarphics@gmail.com> 
+	 * @see       {@link https://wizarphics.com} 
+	 * @copyright Wizarphics 
+	 */
+	public function setRouteArgs(array $routeArgs): self {
+		$this->routeArgs = $routeArgs;
+		return $this;
+	}
 }
--- Router.php
+++ Router.php
@@ -20,17 +20,27 @@ class Router
     public Request $request;
     public Response $response;
     protected array $routes = [];
+
     private array $definedPlaceholder = [
-        '(:num)' => '/^[0-9]+$/',
-        '(:float)' => '/^\d+(\.\d{1,2})?/',
-        '(:any)' => '/^[\w]+$/',
-        '(:alphaL)' => '/^[a-z]+$/',
-        '(:alphaU)' => '/^[A-Z]+$/',
+        '(:num)' => '[0-9]+$',
+        // '(:float)' => '/^\d+(\.\d{1,2})?/',
+        '(:any)' => '[\w]+$',
+        '(:alpha)' => '[a-zA-Z]+$',
+        '(:alphaL)' => '[a-z]+$',
+        '(:alphaU)' => '[A-Z]+$',
+        '(:hex)' => '^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3}])$'
     ];
 
     /**
+     * [Description for __construct]
+     *
      * @param Request $request
      * @param Response $response
+     * 
+     * Created at: 11/24/2022, 2:21:00 PM (Africa/Lagos)
+     * @author     Wizarphics <wizarphics@gmail.com> 
+     * @see       {@link https://wizarphics.com} 
+     * @copyright Wizarphics 
      */
     public function __construct(Request $request, Response $response)
     {
@@ -39,11 +49,35 @@ class Router
     }
 
 
-    public function get($path, $callback)
+
+    /**
+     * [Description for get]
+     *
+     * @param string $path
+     * @param callable|\closure|array $callback
+     * 
+     * @return [type]
+     * 
+     * Created at: 11/24/2022, 2:36:59 PM (Africa/Lagos)
+     * @author     Wizarphics <wizarphics@gmail.com> 
+     * @see       {@link https://wizarphics.com} 
+     * @copyright Wizarphics 
+     */
+    public function get(string $path, callable|\closure|array $callback)
     {
         $this->routes['get'][$path] = $callback;
     }
 
+    /**
+     * [Description for resolve]
+     *
+     * @return \Exception|array|string|void
+     * 
+     * Created at: 11/24/2022, 1:07:04 PM (Africa/Lagos)
+     * @author     Wizarphics <wizarphics@gmail.com> 
+     * @see       {@link https://wizarphics.com} 
+     * @copyright Wizarphics 
+     */
     public function resolve()
     {
         if (empty($this->routes)) {
@@ -51,21 +85,17 @@ class Router
         }
         $path = $this->request->getPath();
         $method = $this->request->Method();
-        if ($path == '/') {
-            $callback = $this->routes[$method][$path] ?? false;
-        } else {
-            if ($method == 'cli') {
-                $callback = $this->handleCliCallback($path);
-            } else {
-                $callback = $this->getCallback($path, $method);
+
+        $callback = $this->routes[$method][$path] ?? false;
+        if ($callback === false) {
+            $callback = $method == 'cli' ? $this->handleCliCallback($path) : $this->getCallback();
+
+            if ($callback === false) {
+                // return $this->renderOnlyView('_errors/_404', []);
+                throw new NotFoundException;
             }
         }
 
-
-        if ($callback === false) {
-            // return $this->renderOnlyView('_errors/_404', []);
-            throw new NotFoundException();
-        }
         if (is_string($callback)) {
             return Application::$app->view->renderView($callback);
         }
@@ -84,7 +114,12 @@ class Router
 
             if (array_key_exists('args', $callback)) {
                 $args = $callback['args'];
-                array_push($args, $this->request, $this->response);
+                if (is_assoc($args)) {
+                    $args['request'] = $this->request;
+                    $args['response'] = $this->response;
+                } else {
+                    array_push($args, $this->request, $this->response);
+                }
                 unset($callback['args']);
 
                 return call_user_func_array($callback, $args);
@@ -94,73 +129,114 @@ class Router
         }
     }
 
-    public function handleCliCallback(string $path)
+
+    /**
+     * [Description for getCallback]
+     *
+     * @param string|null $path
+     * 
+     * @return array|bool
+     * 
+     * Created at: 11/24/2022, 1:08:43 PM (Africa/Lagos)
+     * @author     Wizarphics <wizarphics@gmail.com> 
+     * @see       {@link https://wizarphics.com} 
+     * @copyright Wizarphics 
+     */
+    public function getCallback(?string $path = null): array|bool
     {
-        $path = rtrim($path, '/');
-        $pathArr = explode('/', $path);
-        $routes = $this->routes['cli'] ?? false;
-        if ($routes === false) {
-            return false;
-        }
-        $passArg = $_SERVER['argv'];
-        unset($passArg[0]);
-        $args = array();
-        $callback = '';
-        $placeholderC = 0;
-        $selecteRoute = '';
-        foreach ($routes as $rkey => $value) {
-            if ($rkey == '/') continue;
-            $routeArr = explode('/', $rkey);
-            if (count($routeArr) == count($passArg)) {
-                if ($path == $rkey) {
-                    return $value;
-                } else {
-                    if (array_equality($routeArr, $passArg)) {
-                        return $value;
-                    } else {
-                        $i = 0;
-                        foreach ($routeArr as $key => $value) {
-                            $i++;
-                            echo $i.PHP_EOL, $value.PHP_EOL, $passArg[$key].PHP_EOL;
-                            if (array_key_exists($value, $this->definedPlaceholder)) {
-                                foreach ($this->definedPlaceholder as $pkey => $placeholder) {
-                                    if ($value == $pkey) {
-                                        $placeholderC++;
-                                        if (preg_match($placeholder, $passArg[$key])) {
-                                            $args[] = $passArg[$key];
-                                            $selecteRoute = $rkey;
-                                            echo 'Pattern Matched ' . $placeholder . ' = ' . $pathArr[$key] . PHP_EOL;
-                                        }
-                                    } else {
-                                        // echo 'Pattern gone through 1 not matched.' . $value . ' ' . $passArg[$key] . PHP_EOL;
-                                    }
-                                }
-                            } else {
-                                foreach ($this->definedPlaceholder as $placeholder) {
-                                    if ($value == $placeholder) {
-                                        $placeholderC++;
-                                        if (preg_match($placeholder, $passArg[$key])) {
-                                            array_push($args, $passArg[$key]);
-                                            $selecteRoute = $rkey;
-                                            echo 'Pattern Matched ' . $passArg[$key] . PHP_EOL;
-                                        }
-                                    } else {
-                                        // echo 'Pattern gone through 2 not matched.' . $value . ' ' . $placeholder . ' ' . $key . ' ' . $value . PHP_EOL;
-                                    }
-                                }
-                            }
-                        }
-                    }
+        $path = $path ?? $this->request->getPath();
+        $method = $this->request->Method();
+        // Trim all slashes
+        $url = trim($path, '/');
+
+        //Get all routes for current request
+        $routes = $this->routes[$method] ?? [];
+
+        $routeParams = false;
+        // print '<pre>';
+        // var_dump($routes);
+        // print '</pre>';
+        // exit;
+        //Start iterating over registered routes
+        foreach ($routes as $route => $callback) {
+            // Trim all slashes
+            $route = trim($route, '/');
+
+            // Replace defined placeholders
+            $route = str_replace(array_keys($this->definedPlaceholder), array_values($this->definedPlaceholder), $route);
+            $routeNames = [];
+
+            if (!$route) {
+                continue;
+            }
+
+            // Find all route names from route and save in $routeNames
+            if (preg_match_all('/\{(\w+)(:[^}]+)?}/', $route, $matches)) {
+                $routeNames = $matches[1];
+            }
+
+            // Convert route name into regex pattern
+            $routeRegrex = "@^" . preg_replace_callback('/\{\w+(:([^}]+))?}/', fn ($m) => isset($m[2]) ? "({$m[2]})" : '(\w+)', $route) . "$@";
+
+            // Test and match current route against $routeRegex
+            if (preg_match_all($routeRegrex, $url, $valueMatches)) {
+                $values = [];
+                for ($i = 1; $i < count($valueMatches); $i++) {
+                    $values[] = $valueMatches[$i][0];
                 }
-            } else {
-                dd($routeArr, $passArg);
-                echo count($routeArr) . PHP_EOL, count($passArg);
-                exit;
+                $routeParams = array_combine($routeNames, $values);
+
+
+                $callback['args'] = $routeParams;
+                $this->request->setRouteArgs($routeParams);
+                return $callback;
             }
         }
-        exit;
+
+        return false;
+
+        // throw new NotFoundException();
     }
-    public function getCallback($path, $method)
+
+    /**
+     * [Description for handleCliCallback]
+     *
+     * @param string $path
+     * 
+     * @return array|bool
+     * 
+     * Created at: 11/24/2022, 1:09:33 PM (Africa/Lagos)
+     * @author     Wizarphics <wizarphics@gmail.com> 
+     * @see       {@link https://wizarphics.com} 
+     * @copyright Wizarphics 
+     */
+    public function handleCliCallback(string $path)
+    {
+        $routeArgs = $_SERVER['argv'];
+        unset($routeArgs[0]);
+        $mergePath = array_unique(array_merge($routeArgs, [$path]));
+        $newPath = str_replace('\\', '__cli__', join('/', $mergePath));
+        $callback = $this->getCallback($newPath);
+        return $callback;
+        // \dd(get_defined_vars());
+    }
+
+
+    /**
+     * [Description for getOldCallback]
+     *
+     * @param mixed $path
+     * @param mixed $method
+     * 
+     * @return array|bool
+     * 
+     * Created at: 11/24/2022, 1:09:51 PM (Africa/Lagos)
+     * @author     Wizarphics <wizarphics@gmail.com> 
+     * @see       {@link https://wizarphics.com} 
+     * @copyright Wizarphics 
+     * @deprecated 
+     */
+    public function getOldCallback($path, $method)
     {
         $path = rtrim($path, '/');
         $pathArr = explode('/', $path);
@@ -227,13 +303,72 @@ class Router
         }
     }
 
-    public function post(string $path, $callback)
+    /**
+     * [Description for post]
+     *
+     * @param string $path
+     * @param callable|\closure|array $callback
+     * 
+     * 
+     * Created at: 11/24/2022, 1:10:27 PM (Africa/Lagos)
+     * @author     Wizarphics <wizarphics@gmail.com> 
+     * @see       {@link https://wizarphics.com} 
+     * @copyright Wizarphics 
+     */
+    public function post(string $path, callable|\closure|array $callback)
     {
         $this->routes['post'][$path] = $callback;
     }
 
-    public function cli(string $path, $callback)
+
+    /**
+     * [Description for cli]
+     *
+     * @param string $path
+     * @param callable|\closure|array $callback
+     * 
+     * 
+     * Created at: 11/24/2022, 1:13:43 PM (Africa/Lagos)
+     * @author     Wizarphics <wizarphics@gmail.com> 
+     * @see       {@link https://wizarphics.com} 
+     * @copyright Wizarphics 
+     */
+    public function cli(string $path, callable|\closure|array $callback)
     {
         $this->routes['cli'][$path] = $callback;
+    }
+
+    /**
+     * [Description for getPost]
+     *
+     * @param string $path
+     * 
+     * 
+     * Created at: 11/24/2022, 1:14:08 PM (Africa/Lagos)
+     * @author     Wizarphics <wizarphics@gmail.com> 
+     * @see       {@link https://wizarphics.com} 
+     * @copyright Wizarphics 
+     */
+    public function getPost(string $path, callable|\closure|array $callback)
+    {
+        $this->routes['get'][$path] = $callback;
+        $this->routes['post'][$path] = $callback;
+    }
+
+    /**
+     * [Description for delete]
+     *
+     * @param string $path
+     * @param callable|\closure|array $callback
+     * 
+     * 
+     * Created at: 11/24/2022, 2:20:08 PM (Africa/Lagos)
+     * @author     Wizarphics <wizarphics@gmail.com> 
+     * @see       {@link https://wizarphics.com} 
+     * @copyright Wizarphics 
+     */
+    public function delete(string $path, callable|\closure|array $callback)
+    {
+        $this->routes['delete'][$path] = $callback;
     }
 }
