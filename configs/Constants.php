43bb442cd47d1030e366dcae48f153c6a54f2895
Wizarphics <wizarphics@gmail.com>
2022-12-09 00:00:03 +0100

    Added VIEWPATH and TIME Constants

--- configs/Constants.php
+++ configs/Constants.php
@@ -2,3 +2,22 @@
 
 defined('DEBUG_LOG_PATH') or define('DEBUG_LOG_PATH', ROOT_DIR.'/runtime/logs/debbug_logger/');
 defined('MIGRATION_PATH') or define('MIGRATION_PATH', ROOT_DIR.'/migrations/');
+defined('VIEWPATH') or define('VIEWPATH', ROOT_DIR.'/views/');
+
+
+/*
+ |--------------------------------------------------------------------------
+ | Timing Constants
+ |--------------------------------------------------------------------------
+ |
+ | Provide simple ways to work with the myriad of PHP functions that
+ | require information to be in seconds.
+ */
+defined('SECOND') || define('SECOND', 1);
+defined('MINUTE') || define('MINUTE', 60);
+defined('HOUR')   || define('HOUR', 3600);
+defined('DAY')    || define('DAY', 86400);
+defined('WEEK')   || define('WEEK', 604800);
+defined('MONTH')  || define('MONTH', 2_592_000);
+defined('YEAR')   || define('YEAR', 31_536_000);
+defined('DECADE') || define('DECADE', 315_360_000);
