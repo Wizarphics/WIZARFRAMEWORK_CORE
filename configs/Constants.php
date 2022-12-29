7d5b860a51f36bb0d7349603f17b16dc141f7399
Wizarphics <ireoluwaomtosho7@gmail.com>
2022-12-29 01:46:59 +0100

    added new constant FRAME_WORK_URL and moved the VIEWPATH constant to the bootstrap file

--- configs/Constants.php
+++ configs/Constants.php
@@ -2,7 +2,7 @@
 
 defined('DEBUG_LOG_PATH') or define('DEBUG_LOG_PATH', ROOT_DIR.'/runtime/logs/debbug_logger/');
 defined('MIGRATION_PATH') or define('MIGRATION_PATH', ROOT_DIR.'/migrations/');
-defined('VIEWPATH') or define('VIEWPATH', ROOT_DIR.'/views/');
+define('FRAME_WORK_URL', 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
 
 
 /*
